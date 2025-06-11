e2c963b02d3f131f5ec256603977765c38bbf6d4
AbednegoTenge <abednegotenge180@gmail.com>
2025-06-11 16:21:38 +0000

    setting up user model

---
+++ models/user.js
@@ -0,0 +1,39 @@
+import { sequelize } from "../config/db";
+import { DataTypes } from "sequelize";
+
+const User = sequelize.define(
+    'User', {
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [6, 100], // Minimum length of 6 characters
+            }
+        }
+
+    }
+)
+
+
+const initUserModel = async () => {
+    try {
+        await User.sync({ force: false }); // Set force to true to drop the table if it exists
+        console.log("User model synced successfully.");
+    } catch (error) {
+        console.error("Error syncing User model:", error);
+    }
+};
+
+export default { User, initUserModel };
