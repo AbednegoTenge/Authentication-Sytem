2b2daf2abfb5042c9aefb131cb9ce416bebaa060
AbednegoTenge <abednegotenge180@gmail.com>
2025-06-11 18:41:25 +0000

    parsing routes into server

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { connectDB } from './config/db.js';
 import { initUserModel } from './models/user.js';
+import authRoutes from './routes/authRoutes.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,6 +13,10 @@ const startServer = async () => {
         await initUserModel();
 
         app.use(express.json());
+        app.use(cookieParser());
+
+        // Set up routes
+        app.use('/', authRoutes);
 
         app.get('/', (req, res) => {
             res.send('Hello, World!');
